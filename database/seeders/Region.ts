db0c29d31b13c0abda7733ac544050021ec3f13a
haniialfiyyaah <hanii.alfiyyah@gmail.com>
2021-10-14 18:46:54 +0700

    Add search nearby

--- database/seeders/Region.ts
+++ database/seeders/Region.ts
@@ -8,21 +8,21 @@ export default class RegionSeeder extends BaseSeeder {
   public async run() {
     /* kabupaten */
     // let cities = await readFile('data-kabupaten.json')
-    const { data: kab }: { data } = await axios.get(`${URL_KAB}`)
+    const { data: kab }: { data } = await axios.get(`${URL_KAB}?limit=6000`)
     let cities = kab?.data
     cities = filterWilayah(cities, 'kota', 'Kabupaten/Kota')
     await Region.createMany(cities)
 
     /* kecamatan */
     // let districs = await readFile('data-kecamatan.json')
-    const { data: kec }: { data } = await axios.get(`${URL_KEC}`)
+    const { data: kec }: { data } = await axios.get(`${URL_KEC}?limit=1000`)
     let districs = kec?.data
     districs = filterWilayah(districs, 'kecamatan', 'Kecamatan')
     await Region.createMany(districs)
 
     /* kelurahan */
     // let villages = await readFile('data-kelurahan.json')
-    const { data: kel }: { data } = await axios.get(`${URL_KEL}`)
+    const { data: kel }: { data } = await axios.get(`${URL_KEL}?limit=50`)
     let villages = kel?.data
     villages = filterWilayah(villages, 'kelurahan', 'Kelurahan/Desa')
     await Region.createMany(villages)
--- start/routes.ts
+++ start/routes.ts
@@ -19,7 +19,66 @@
 */
 
 import Route from '@ioc:Adonis/Core/Route'
+import Database from '@ioc:Adonis/Lucid/Database'
+import Region from 'App/Models/Region'
+import axios from 'axios'
+import { URL_KEL } from 'Config/constants'
+import { string } from '@ioc:Adonis/Core/Helpers'
 
 Route.get('/', async () => {
-  return { hello: 'world' }
+  //
+})
+
+Route.get('/search', async ({ request }) => {
+  interface ResponseSearch {
+    id: number
+    name: string
+    category_id: number
+    city_name: string
+    district_name: string
+    latitude: number
+    longitude: number
+  }
+
+  const { longitude, latitude, category_id } = request.qs()
+  // error required
+  if (!longitude || !latitude) {
+    return {
+      status: 'error',
+      message: 'Longitude and latitude is required as query',
+    }
+  }
+  // search location
+  const { rows } = await Database.rawQuery(`
+    select * from (SELECT  *,
+    ( 5000 * acos( cos( radians(${latitude}) ) * cos( radians( latitude ) ) * cos( radians( longitude )
+    - radians(${longitude}) ) + sin( radians(${latitude}) ) * sin( radians( latitude ) ) ) ) AS distance 
+    FROM places ${category_id ? `where category_id=${category_id}` : ''} ) al 
+    where distance < 5
+    ORDER BY distance`)
+
+  let result: ResponseSearch[] = []
+  for (const i in rows) {
+    const row = rows[i]
+    const { kemendagri_code, level } = await Region.findOrFail(row.region_id)
+    const type =
+      level === 'Kecamatan'
+        ? 'kemendagri_kecamatan_kode'
+        : level === 'Kabupaten/Kota'
+        ? 'kemendagri_kelurahan_kode'
+        : 'kemendagri_kota_kode'
+    const { data }: { data } = await axios.get(
+      `${URL_KEL}?limit=1&${type}=${kemendagri_code}`
+    )
+    result.push({
+      id: row.id,
+      name: row.name,
+      category_id: row.category_id,
+      city_name: string.capitalCase(data?.data[0].bps_kota_nama),
+      district_name: string.capitalCase(data?.data[0].bps_kecamatan_nama),
+      latitude: row.latitude,
+      longitude: row.longitude,
+    })
+  }
+  return result
 })
